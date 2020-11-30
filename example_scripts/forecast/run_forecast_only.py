9357973d2eb2a642ad700b3af85a8162e8d195b8
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2020-11-30 14:43:53 +0100

    Added example scripts for experiments

---
+++ example_scripts/forecast/run_forecast_only.py
@@ -0,0 +1,49 @@
+#!/bin/env python
+# -*- coding: utf-8 -*-
+#
+#Created on 20.03.17
+#
+#Created for py_bacy
+#
+#@author: Tobias Sebastian Finn, tobias.sebastian.finn@studium.uni-hamburg.de
+#
+#    Copyright (C) {2017}  {Tobias Sebastian Finn}
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# System modules
+import logging
+import os
+
+# External modules
+
+# Internal modules
+from py_bacy.cycle import Cycle
+from py_bacy.int2lm import Int2lmModule
+from py_bacy.cosmo import CosmoModule
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+    configs = '.'
+    cycle = Cycle(os.path.join(configs, 'cycle.yml'))
+    cycle.register_model(Int2lmModule, 'int2lm_hh', None,
+                         config_path=os.path.join(configs, 'int2lm_hh.yml'))
+    cycle.register_model(CosmoModule, 'cosmo_hh', 'int2lm_hh',
+                         config_path=os.path.join(configs, 'cosmo_hh.yml'))
+    cycle.run()
+
+if __name__ == '__main__':
+    main()
+
