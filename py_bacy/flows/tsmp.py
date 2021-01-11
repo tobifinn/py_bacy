ffc34897001fb2fa92ae5da4fe52a31630f74600
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-01-11 17:17:33 +0100

    Added function to get terrsysmp flow

--- py_bacy/flows/tsmp.py
+++ py_bacy/flows/tsmp.py
@@ -28,71 +28,73 @@ from py_bacy.tasks.tsmp import *
 logger = logging.getLogger(__name__)
 
 
-with Flow('TerrSysMP model run') as tsmp_run:
-    start_time = Parameter('start_time')
-    end_time = Parameter('end_time')
-    config_path = Parameter('config_path')
-    cycle_config = Parameter('cycle_config')
-    restart = Parameter('restart')
-    name = Parameter('name')
-    parent_output = Parameter('parent_output', default=None)
+def get_tsmp_flow():
+    with Flow('TerrSysMP model run') as tsmp_run:
+        start_time = Parameter('start_time')
+        end_time = Parameter('end_time')
+        config_path = Parameter('config_path')
+        cycle_config = Parameter('cycle_config')
+        restart = Parameter('restart')
+        name = Parameter('name')
+        parent_output = Parameter('parent_output', default=None)
 
-    config_reader = ReadInConfig()
-    tsmp_config = config_reader(config_path)
+        config_reader = ReadInConfig()
+        tsmp_config = config_reader(config_path)
 
-    rundir_constructor = RundirConstructor()
-    run_dir = rundir_constructor(name, start_time, cycle_config)
+        rundir_constructor = RundirConstructor()
+        run_dir = rundir_constructor(name, start_time, cycle_config)
 
-    create_replacement = CreateTSMPReplacement()
-    placeholder_dict = create_replacement(
-        name, start_time, end_time, run_dir, tsmp_config, cycle_config
-    )
+        create_replacement = CreateTSMPReplacement()
+        placeholder_dict = create_replacement(
+            name, start_time, end_time, run_dir, tsmp_config, cycle_config
+        )
 
-    namelist_modifier = ModifyNamelist()
-    namelist = namelist_modifier(tsmp_config, placeholder_dict)
+        namelist_modifier = ModifyNamelist()
+        namelist = namelist_modifier(tsmp_config, placeholder_dict)
 
-    ens_constructor = ConstructEnsemble()
-    ens_suffix, ens_range = ens_constructor(cycle_config)
+        ens_constructor = ConstructEnsemble()
+        ens_suffix, ens_range = ens_constructor(cycle_config)
 
-    folder_creator = CreateDirectoryStructure(
-        directories=['input', 'output', 'analysis']
-    )
-    created_folders = folder_creator.map(unmapped(run_dir), ens_suffix)
+        folder_creator = CreateDirectoryStructure(
+            directories=['input', 'output', 'analysis']
+        )
+        created_folders = folder_creator.map(unmapped(run_dir), ens_suffix)
 
-    parent_getter = ParentGetter()
-    parent_model_output = parent_getter.map(
-        unmapped(cycle_config),
-        unmapped(run_dir),
-        ens_suffix,
-        unmapped(parent_output)
-    )
+        parent_getter = ParentGetter()
+        parent_model_output = parent_getter.map(
+            unmapped(cycle_config),
+            unmapped(run_dir),
+            ens_suffix,
+            unmapped(parent_output)
+        )
 
-    data_linker = TSMPDataLinking()
-    input_folders = data_linker.map(
-        created_folders=created_folders,
-        parent_model_output=parent_model_output,
-        start_time=unmapped(start_time),
-        tsmp_config=unmapped(tsmp_config),
-        restart=unmapped(restart)
-    )
+        data_linker = TSMPDataLinking()
+        input_folders = data_linker.map(
+            created_folders=created_folders,
+            parent_model_output=parent_model_output,
+            start_time=unmapped(start_time),
+            tsmp_config=unmapped(tsmp_config),
+            restart=unmapped(restart)
+        )
 
-    namelist_initializer = InitializeNamelist('tsmp_run.nml')
-    execution_scripts = namelist_initializer.map(
-        namelist=unmapped(namelist),
-        input_folder=input_folders,
-        mem=ens_range
-    )
+        namelist_initializer = InitializeNamelist('tsmp_run.nml')
+        execution_scripts = namelist_initializer.map(
+            namelist=unmapped(namelist),
+            input_folder=input_folders,
+            mem=ens_range
+        )
 
-    slurm_checker = CheckSlurmRuns()
-    output_folders = slurm_checker(
-        run_dir, folders=created_folders,
-        upstream_tasks=[execution_scripts]
-    )
+        slurm_checker = CheckSlurmRuns()
+        output_folders = slurm_checker(
+            run_dir, folders=created_folders,
+            upstream_tasks=[execution_scripts]
+        )
 
-    output_checker = CheckOutput([
-        'clmoas.clm2.h0.*.nc',
-        'clmoas.clm2.r.*.nc',
-        'lffd*.nc_ana',
-        'lfff*.nc_fg'
-    ])
-    flow_output = output_checker.map(output_folders)
+        output_checker = CheckOutput([
+            'clmoas.clm2.h0.*.nc',
+            'clmoas.clm2.r.*.nc',
+            'lffd*.nc_ana',
+            'lfff*.nc_fg'
+        ])
+        flow_output = output_checker.map(output_folders)
+    return tsmp_run, flow_output
