eff1f722a00aae19e7a2f90d87683a7574a40d80
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-01-13 14:46:07 +0100

    Renamed flow to external_flow

--- py_bacy/tasks/utils.py
+++ py_bacy/tasks/utils.py
@@ -24,14 +24,14 @@ logger = logging.getLogger(__name__)
 
 
 @task
-def run_external_flow(flow: Flow, **kwargs) -> Any:
+def run_external_flow(external_flow: Flow, **kwargs) -> Any:
     """
     This task runs a given external prefect.Flow with given additional
     keyword arguments.
 
     Parameters
     ----------
-    flow : prefect.Flow
+    external_flow : prefect.Flow
         This prefect flow is run with given additional keyword arguments.
     **kwargs
         These additional keyword arguments are passed to the run method of
@@ -41,4 +41,4 @@ def run_external_flow(flow: Flow, **kwargs) -> Any:
     -------
     The output of flow's run method is returned.
     """
-    return flow.run(**kwargs)
+    return external_flow.run(**kwargs)
