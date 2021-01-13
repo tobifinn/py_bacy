9da02c1d5144e9f7edac5eda6838c39c41335f02
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-01-13 12:16:18 +0100

    Improved general tasks

--- py_bacy/tasks/general.py
+++ py_bacy/tasks/general.py
@@ -17,7 +17,7 @@ import os.path
 
 # External modules
 import prefect
-from prefect import Task, Flow
+from prefect import task, Flow
 
 import yaml
 
@@ -25,56 +25,55 @@ import yaml
 
 
 __all__ = [
-    'ReadInConfig',
-    'ParentGetter'
+    'config_reader',
+    'get_parent_output',
+    'run_external_flow'
 ]
 
 
-class ReadInConfig(Task):
-    def run(self, config_path: Union[None, str]) -> Dict[str, Any]:
-        if config_path is None:
-            self.logger.info(
-                'No config path given, I will return an empty config dictionary'
+@task
+def config_reader(config_path: Union[None, str]) -> Dict[str, Any]:
+    if config_path is None:
+        prefect.context.logger.info(
+            'No config path given, I will return an empty config dictionary'
+        )
+        return dict()
+    try:
+        yaml_file = open(config_path, 'r')
+        config = yaml.load(yaml_file, Loader=yaml.FullLoader)
+        yaml_file.close()
+    except FileNotFoundError as e:
+        prefect.context.logger.error(
+            'The config file {0:s} couldn\'t be found'.format(
+                config_path
             )
-            return dict()
-        try:
-            yaml_file = open(config_path, 'r')
-            config = yaml.load(yaml_file, Loader=yaml.FullLoader)
-            yaml_file.close()
-        except FileNotFoundError as e:
-            self.logger.error(
-                'The config file {0:s} couldn\'t be found'.format(
-                    config_path
-                )
-            )
-            raise FileNotFoundError(e)
-        return config
+        )
+        raise FileNotFoundError(e)
+    return config
 
 
-class ParentGetter(Task):
-    def run(
-            self,
-            cycle_config: Dict[str, Any],
-            run_dir: str,
-            ens_suffix: str,
-            parent_output: Union[str, None] = None
-    ) -> str:
-        outer_dir = os.path.dirname(run_dir)
-        analysis_dir = os.path.join(outer_dir, 'analysis')
-        if parent_output:
-            parent_path = parent_output
-        elif os.path.isdir(analysis_dir):
-            parent_path = analysis_dir
-        else:
-            parent_path = cycle_config['EXPERIMENT']['path_init']
-        parent_path = os.path.join(parent_path, ens_suffix)
-        return parent_path
+@task
+def get_parent_output(
+        cycle_config: Dict[str, Any],
+        run_dir: str,
+        ens_suffix: str,
+        parent_output: Union[str, None] = None
+) -> str:
+    outer_dir = os.path.dirname(run_dir)
+    analysis_dir = os.path.join(outer_dir, 'analysis')
+    if parent_output:
+        parent_path = parent_output
+        prefect.context.logger.debug('Parent model output given')
+    elif os.path.isdir(analysis_dir):
+        parent_path = analysis_dir
+        prefect.context.logger.debug('Analysis directory given')
+    else:
+        parent_path = cycle_config['EXPERIMENT']['path_init']
+        prefect.context.logger.debug('Initial data directory given')
+    parent_path = os.path.join(parent_path, ens_suffix)
+    return parent_path
 
 
-class FlowRunner(Task):
-    def __init__(self, flow: Flow, **kwargs):
-        super().__init__(**kwargs)
-        self.flow = flow
-
-    def run(self, **kwargs) -> str:
-        return self.flow.run(**kwargs)
+@task
+def run_external_flow(flow: Flow, **kwargs) -> Any:
+    return flow.run(**kwargs)
