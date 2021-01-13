d38ceddae80289e3b4ce2f29f77bdf35c5d083b4
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-01-13 13:05:17 +0100

    Added docstrings to system tasks

--- py_bacy/tasks/system.py
+++ py_bacy/tasks/system.py
@@ -13,11 +13,11 @@
 # System modules
 import logging
 import os
-from typing import Any, Dict
-import datetime
+from typing import Iterable, List
+import tempfile
 
 # External modules
-from prefect import Task, task
+from prefect import task, context
 
 # Internal modules
 
@@ -25,62 +25,114 @@ from prefect import Task, task
 logger = logging.getLogger(__name__)
 
 
-class CreateFolders(Task):
-    def __init__(self, template: str, **kwargs):
-        super().__init__(**kwargs)
-        self.template = template
+__all__ = [
+    'symlink',
+    'create_folders',
+    'create_directory_structure',
 
-    def run(self, **kwargs) -> str:
-        initialized_path = self.template.format(**kwargs)
-        if not os.path.isdir(initialized_path):
-            os.makedirs(initialized_path)
-        if not os.path.isdir(initialized_path):
-            raise OSError(
-                'Couldn\'t initialize the directory path {0:s}'.format(
-                    initialized_path
-                )
+]
+
+
+@task
+def symlink(source: str, target: str) -> str:
+    """
+    Symlink a given source path to given target path. This is done with an
+    atomic operation. If the target path already exists, it will be
+    overwritten by the symbolic link.
+
+    Parameters
+    ----------
+    source : str
+        This is the source path, which have to be available.
+    target : str
+        This is the target path, which will be overwritten if it already exists.
+
+    Returns
+    -------
+    target: str
+        The path to the linked target.
+
+    Raises
+    ------
+    ValueError
+        A ValueError is raised if the source path does not exists.
+    """
+    if not os.path.exists(source):
+        raise ValueError(
+            'Give source path {0:s} doesn\'t exists!'.format(
+                source
             )
-        return initialized_path
-
-
-class SymbolicLinking(Task):
-    def __init__(self, source_template: str, target_template: str, **kwargs):
-        super().__init__(**kwargs)
-        self.source_template = source_template
-        self.target_template = target_template
-
-    def run(self, **kwargs) -> str:
-        initialized_source = self.source_template.format(**kwargs)
-        initialized_target = self.target_template.format(**kwargs)
-        if not os.path.exists(initialized_source):
-            raise ValueError(
-                'Give source path {0:s} doesn\'t exists!'.format(
-                    initialized_source
-                )
-            )
-        if os.path.isfile(initialized_target):
-            self.logger.debug(
-                'The target path {0:s} already exists, '
-                'I\'ll remove the file'.format(
-                    initialized_target
-                )
-
-            )
-            os.remove(initialized_target)
-        os.symlink(initialized_source, initialized_target)
-        return initialized_target
-
-
-class RundirConstructor(Task):
-    def run(
-            self,
-            model_name: str,
-            time: datetime.datetime,
-            cycle_config: Dict[str, Any]
-    ) -> str:
-        run_dir = os.path.join(
-            cycle_config['EXPERIMENT']['path'],
-            time.strftime('%Y%m%d_%H%M'),
-            model_name
         )
-        return run_dir
+    context.logger.debug('Symlink: {0:s} -> {1:s}'.format(source, target))
+    temp_name = next(tempfile._get_candidate_names())
+    tmp_file = os.path.join(os.path.dirname(target), temp_name)
+    os.symlink(source, tmp_file)
+    os.replace(tmp_file, target)
+    return target
+
+
+@task
+def create_folders(dir_path: str) -> str:
+    """
+    Create folder structure with given path.
+
+    Parameters
+    ----------
+    dir_path : str
+        This folder path is created.
+
+    Returns
+    -------
+    dir_path : str
+        The created folder path.
+
+    Raises
+    ------
+    OSError
+        An OSError is raised if after creation the directory path is not
+        available.
+    """
+    if not os.path.isdir(dir_path):
+        os.makedirs(dir_path)
+    if not os.path.isdir(dir_path):
+        raise OSError(
+            'Couldn\'t initialize the directory path {0:s}'.format(
+                dir_path
+            )
+        )
+    return dir_path
+
+
+@task
+def create_directory_structure(
+        run_dir: str,
+        ens_suffix: str,
+        directories: Iterable[str]
+) -> List[str]:
+    """
+    Construct a directory structure for PyBaCy. The structure is
+    iteratively created by: `run_dir/dir/ens_suffix` where dir is the
+    iteratively selected directory given by the `directories` keyword.
+
+    Parameters
+    ----------
+    run_dir : str
+        This is the basis directory, where the ensemble suffixes and
+        directories are created
+    ens_suffix : str
+        This is the ensemble suffix that is appended to given run dir and
+        directory.
+    directories : Iterable[str]
+        These directories are created with this function.
+
+    Returns
+    -------
+    created_directories : List[str]
+        These are the paths to the created directories.
+    """
+    created_directories = []
+    for dir_name in directories:
+        dir_path = os.path.join(run_dir, dir_name, ens_suffix)
+        _ = create_folders(dir_path)
+        created_directories.append(dir_path)
+    return created_directories
