b1f2d6e53be53ad9c135ddd6ddcaaa333fc13de6
Tobias Finn <tobias.sebastian.finn@uni-hamburg.de>
2021-01-30 19:21:32 +0100

    Added debug messages for str

--- py_bacy/tasks/slurm.py
+++ py_bacy/tasks/slurm.py
@@ -35,10 +35,19 @@ __all__ = [
 def submit_script(
         script_path: str
 ) -> str:
+    logger = prefect.context.get('logger')
+    logger.debug('Call {0:s}'.format(script_path))
     call_args = ['sbatch', script_path]
+    logger.debug('Submit {0}'.format(call_args))
     output = subprocess.run(call_args, capture_output=True)
     output_str = output.stdout.decode('utf-8')
+    logger.debug(
+        'Got {0:s} as output for {1:s}'.format(output_str, script_path)
+    )
     pid_str = re.findall(r'\b\d+\b', output_str)[0]
+    logger.debug(
+        'Got {0:s} as pid for {1:s}'.format(pid_str, script_path)
+    )
     return pid_str
 
 
@@ -47,19 +56,19 @@ def check_slurm_running(
         pids: List[str],
         sleep_time: float = 5.0
 ) -> Dict[str, bool]:
-        pids_str = ','.join(pids)
-        pids_running = {pid: True for pid in pids}
-        while any(pids_running.values()):
-            time.sleep(sleep_time)
-            squeue_output = subprocess.check_output(
-                ['squeue', '--jobs={0:s}'.format(pids_str)], text=True
-            )
-            pids_running = {pid: pid in squeue_output for pid in pids}
-            logger = prefect.context.get('logger')
-            logger.info('Still runnning {0:d}/{1:d}'.format(
-                int(np.sum(list(pids_running.values()))), len(pids_running)
-            ))
-            logger.debug(
-                'Running PIDS: {0}'.format(pids_running)
-            )
-        return pids_running
+    logger = prefect.context.get('logger')
+    pids_str = ','.join(pids)
+    pids_running = {pid: True for pid in pids}
+    while any(pids_running.values()):
+        time.sleep(sleep_time)
+        squeue_output = subprocess.check_output(
+            ['squeue', '--jobs={0:s}'.format(pids_str)], text=True
+        )
+        pids_running = {pid: pid in squeue_output for pid in pids}
+        logger.info('Still runnning {0:d}/{1:d}'.format(
+            int(np.sum(list(pids_running.values()))), len(pids_running)
+        ))
+        logger.debug(
+            'Running PIDS: {0}'.format(pids_running)
+        )
+    return pids_running
